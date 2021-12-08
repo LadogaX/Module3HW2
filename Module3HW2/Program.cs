5343f183464c88363ea269a295e861d518d95d81
Artem <LadogaX@gmail.com>
2021-12-08 14:37:04 +0200

    Empty project

---
+++ Module3HW2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+namespace Module3HW2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
